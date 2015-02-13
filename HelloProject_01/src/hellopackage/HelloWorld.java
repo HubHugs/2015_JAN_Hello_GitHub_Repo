21e8a66960f0daf1a84ebbe82b276d062533435a
Mr Spyrius <spyrius@hotmail.com>
2015-02-13 12:50:40 +1000

    Started fleshing out HelloWorld class

--- HelloProject_01/src/hellopackage/HelloWorld.java
+++ HelloProject_01/src/hellopackage/HelloWorld.java
@@ -1,10 +1,21 @@
 package hellopackage;
 
 public class HelloWorld {
+	
+	private String name;
+
+	public HelloWorld(String name) {
+		this.name = name;
+	}
+	
+	public void mrSpyriusMessage() {
+		String message = "Why, hello there!";
+		System.out.println(name + " says: " + message);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		HelloWorld hw = new HelloWorld("Mr. Spyrius");
+		hw.mrSpyriusMessage();
 	}
 
 }
